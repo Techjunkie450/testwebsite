6d6144f2b7a6c6fdbcc2a966e3bfc0c8ce114856
Techjunkie450 <techjunkie450@gmail.com>
2025-08-01 23:20:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/testwebsite/', // <-- Replace with your GitHub repo name
+  base: '/', // <-- Replace with your GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
